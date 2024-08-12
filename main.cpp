178775750c071589ff9591084cd605e3aa1e755b
Keo Ponleou Sok <ponleousk@gmail.com>
2024-08-12 18:16:34 +1000

    fix: made hitbox and adjust character positioning

--- main.cpp
+++ main.cpp
@@ -43,10 +43,10 @@ struct room_properties_data
 struct room_data
 {
     tile_data floor_array[ROOM_HEIGHT][ROOM_WIDTH];
-    // color color_pattern[2]; // TODO: if this is not needed to be used, remove it because its already it a functioin for each npc room
+    color color_pattern[3]; // 0 and 1 is the floor checkers color pattern, 2 is the walls
     int size_y = ROOM_HEIGHT;
     int size_x = ROOM_WIDTH;
-    coordinate spawn_coords = {ROOM_WIDTH / 2, ROOM_HEIGHT - 1};
+    coordinate spawn_coords = {(int)ROOM_WIDTH / 2, (int)ROOM_HEIGHT - 3}; // -1 for the wall, -2 for the character's height
     room_properties_data properties;
 };
 
@@ -67,12 +67,6 @@ int coordinates_to_pixel(int coord)
 
 void build_room(room_data &room)
 {
-    color slate_grey = rgb_color(112, 128, 144);
-    color light_slate_grey = rgb_color(132, 144, 153);
-
-    // npc_room.floor.color_pattern[0] = softBlue;
-    // npc_room.floor.color_pattern[1] = paleYellow; //TODO: remove if not needed
-
     int size_y = room.size_y;
     int size_x = room.size_x;
 
@@ -80,12 +74,20 @@ void build_room(room_data &room)
     {
         for (int x = 0; x < size_x; x++)
         {
-            color first_color = slate_grey;
-            color second_color = light_slate_grey;
+
+            if (x == 0 || x == size_x - 1 || y == 0 || y == size_y - 1)
+            {
+                room.floor_array[y][x].tile_color = room.color_pattern[3];
+                room.floor_array[y][x].passable = false;
+                continue;
+            }
+
+            color first_color = room.color_pattern[0];
+            color second_color = room.color_pattern[1];
             if (y % 2 != 0)
             {
-                first_color = light_slate_grey;
-                second_color = slate_grey;
+                first_color = room.color_pattern[1];
+                second_color = room.color_pattern[0];
             }
 
             if (x % 2 == 0)
@@ -96,8 +98,17 @@ void build_room(room_data &room)
     }
 }
 
+// TODO: have it for each type of room
 void build_npc_room(room_data &room)
 {
+    color slate_grey = rgb_color(112, 128, 144);
+    color light_slate_grey = rgb_color(132, 144, 153);
+    color light_steel_blue = rgb_color(150, 170, 200);
+
+    room.color_pattern[0] = slate_grey;
+    room.color_pattern[1] = light_slate_grey;
+    room.color_pattern[2] = light_steel_blue;
+
     build_room(room);
     room.properties.is_npc_room = true;
 }
@@ -127,7 +138,7 @@ void draw_floor(const room_data &room)
     }
 }
 
-// TODO: access properties to draw different things for rooms with different properties (wull probablt)
+// TODO: access properties to draw different things for rooms with different properties, and will access more functiosn to add the stuff for different rooms
 void draw_room(const room_data &room)
 {
     draw_floor(room);
@@ -152,13 +163,25 @@ void load_player(player_data &player, const room_data &room)
     }
 
     player.coords = room.spawn_coords;
+
+    player.hitbox = {(double)player.coords.x, (double)player.coords.y, model_width * player.model_scaling, model_height * player.model_scaling};
+    write_line(std::to_string(player.coords.y));
 }
 
 void draw_player(const player_data &player)
 {
-    int pos_x = coordinates_to_pixel(player.coords.x);
-    int pos_y = coordinates_to_pixel(player.coords.y);
+    double model_width = bitmap_width(player.player_model);
+    double model_height = bitmap_height(player.player_model);
+    double scaling = player.model_scaling;
+
+    int pos_x = coordinates_to_pixel(player.coords.x) + ((model_width * scaling) - model_width) / 2;
+    int pos_y = coordinates_to_pixel(player.coords.y) + ((model_height * scaling) - model_height) / 2;
     draw_bitmap(player.player_model, pos_x, pos_y, option_scale_bmp(player.model_scaling, player.model_scaling));
+
+    const rectangle &hitbox = player.hitbox;
+    int hitbox_pos_x = coordinates_to_pixel(hitbox.x);
+    int hitbox_pos_y = coordinates_to_pixel(hitbox.y);
+    draw_rectangle(color_red(), hitbox_pos_x, hitbox_pos_y, hitbox.width, hitbox.height);
 }
 
 int main()
